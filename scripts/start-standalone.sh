1004ff89ebc55c3affb8526e1e9ccdf23c44af19
gowri <you@example.com>
2018-12-20 14:49:14 +0530

    Adding utxoTx Route

---
+++ scripts/start-standalone.sh
@@ -0,0 +1,4 @@
+src/stellar-core --conf config/node_standalone/stellar-core.cfg --newdb
+src/stellar-core --conf config/node_standalone/stellar-core.cfg --newhist
+src/stellar-core --conf config/node_standalone/stellar-core.cfg --forcescp
+src/stellar-core --conf config/node_standalone/stellar-core.cfg
